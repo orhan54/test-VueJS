19ce2a9ed51571a67a160f02b32937f3b70faea4
CICEK Orhan <orhancicek1985@gmail.com>
2026-02-06 12:08:35 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <li> {{ person.firstname }}</li>
+    <li> {{ person.lastname }}</li>
+    <li> {{ person.age }}</li>
+  </ul>
+
+  <button @click.prevent="randomAge">Changer age</button>
+</template>
+
+
+<script setup>
+import {ref} from 'vue'
+
+const person = ref({
+  firstname: 'john',
+  lastname: "Doe",
+  age: 20
+})
+
+const randomAge = () => {
+  person.value.age = Math.round(Math.random() * 100)
+}
+
+</script>
